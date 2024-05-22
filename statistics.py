abffd37edceff48e06bfa9158414c61621d66d9e
kbason <166721865+kbason@users.noreply.github.com>
2024-05-22 20:15:36 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -10,3 +10,18 @@ class Stats:
         self.games_played = 0
         self.best_score = 0
         self.current_score = 0
+
+    def update_stats(self, score):
+        # Aktualizacja statystyk po zakończeniu gry
+        self.games_played += 1
+        self.current_score = score
+        if score > self.best_score:
+            self.best_score = score
+
+    def get_stats(self):
+        # Pobranie bieżących statystyk
+        return {
+            "games_played": self.games_played,
+            "best_score": self.best_score,
+            "current_score": self.current_score
+        }
